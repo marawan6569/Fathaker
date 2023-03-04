9e391ca82fb8706d4bf1d9d20d1690e83a5ecc1b
marawan6569 <marawan6569@gmail.com>
2023-03-04 16:19:31 +0200

    add _backup_to_folder to backup.py

--- backup.py
+++ backup.py
@@ -3,13 +3,27 @@ import os
 
 class Backup:
     def __init__(self,):
-        self.generate_backups_file()
+        self._generate_backups_file()
+        self._backup_to_folder()
 
     @staticmethod
-    def generate_backups_file():
+    def _generate_backups_file():
         if not os.path.isdir('db_backup'):
             os.mkdir('db_backup')
         os.system(f'python3 manage.py dumpdata --indent 4 > db_backup/backup.json')
+
+    @staticmethod
+    def _backup_to_folder():
+        if not os.path.isdir('../backup'):
+            os.mkdir('../backup')
+
+        if os.path.isdir('../backup/db_backup/'):
+            os.system('rm -r ../backup/db_backup/')
+        if os.path.isdir('../backup/media/'):
+            os.system('rm -r ../backup/media/')
+
+        os.system('cp -r db_backup/ ../backup/db_backup/')
+        os.system('cp -r media/ ../backup/media/')
 
 
 if __name__ == '__main__':
