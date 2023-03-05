9e7a18085ed36adcc229036499ae9be3ff12103c
marawan6569 <marawan6569@gmail.com>
2023-03-05 23:30:21 +0200

    add Surah and Verse models to admin panel

--- verses/admin.py
+++ verses/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from  .models import Verse, Surah
 
 # Register your models here.
+
+
+@admin.register(Verse)
+class VerseAdmin(admin.ModelAdmin):
+
+    list_display = ['__str__', 'verse_pk', 'surah', 'number_in_quran', 'page', 'the_quarter', 'juz', 'is_sajda']
+    list_filter = ['surah', 'juz', 'the_quarter', 'page', 'is_sajda']
+    search_fields = ['verse', 'verse_without_tashkeel']
+
+
+@admin.register(Surah)
+class SurahAdmin(admin.ModelAdmin):
+    list_display = ['name', 'name_without_tashkeel']
+    search_fields = ['name', 'name_without_tashkeel']
