5095ba3118cb614ea9cbcea9cdc96e627888f45b
marawan6569 <marawan6569@gmail.com>
2023-03-24 16:43:58 +0200

    add test for NavLink

--- core/tests.py
+++ core/tests.py
@@ -8,7 +8,7 @@ class NavLinkTest(TestCase):
     def setUp(self) -> None:
         home = NavLink.objects.create(link_type=0, name="home", display_name="home", destination="core:homepage")
         github = NavLink.objects.create(link_type=2, name="github", display_name="home",
-                                        destination="https://github.com/")
+                                        destination="https://github.com/", is_external=True)
         home.save()
         github.save()
 
@@ -17,3 +17,11 @@ class NavLinkTest(TestCase):
         github = NavLink.objects.get(name="github")
         self.assertEqual(home.__str__(), "home")
         self.assertEqual(github.__str__(), "github")
+
+    def test_get_link(self):
+        home = NavLink.objects.get(name="home")
+        self.assertEqual(home.get_link(), "/")
+
+    def test_get_link_if_is_external(self):
+        github = NavLink.objects.get(name="github")
+        self.assertEqual(github.get_link(), "https://github.com/")
