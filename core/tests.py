6762440048b0148ff43e640f7f0556469e295b8b
marawan6569 <marawan6569@gmail.com>
2023-03-24 16:40:28 +0200

    add test for NavLink

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from .models import NavLink
 # Create your tests here.
+
+
+class NavLinkTest(TestCase):
+
+    def setUp(self) -> None:
+        home = NavLink.objects.create(link_type=0, name="home", display_name="home", destination="core:homepage")
+        github = NavLink.objects.create(link_type=2, name="github", display_name="home",
+                                        destination="https://github.com/")
+        home.save()
+        github.save()
+
+    def test_nav_link__str__(self):
+        home = NavLink.objects.get(name="home")
+        github = NavLink.objects.get(name="github")
+        self.assertEqual(home.__str__(), "home")
+        self.assertEqual(github.__str__(), "github")
