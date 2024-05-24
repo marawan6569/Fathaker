87f17fc68e3a45d53f60b8cbdb072e6ce12f3a4e
marwanfazora <marawan6569@gmail.com>
2024-05-25 00:05:45 +0300

    update admin panel title

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import NavLink
 
+admin.site.site_header = "Fathaker administration"
+admin.site.site_title = "Fathaker administration"
+admin.site.index_title = "Fathaker administration"
+
+
 # Register your models here.
 
 
