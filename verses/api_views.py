16377c21dee0196a553ab79d88dce46e963bd640
marawan6569 <marawan6569@gmail.com>
2026-02-09 06:13:32 +0200

    add multiple API endpoints for searching and retrieving verses

--- verses/api_views.py
+++ verses/api_views.py
@@ -1,9 +1,13 @@
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Verse
 from .serializers import VerseSerializer
 
 
+# #1
 class VersesList(ListAPIView):
     """
         Endpoint To Get all verses
@@ -19,4 +23,81 @@ class VersesList(ListAPIView):
             query = query.filter(page=page)
 
         return Response(VerseSerializer(query, many=True).data)
+
+
+# #2
+class VersesSearch(ListAPIView):
+    """Search verses by keyword using ?q=keyword"""
+    serializer_class = VerseSerializer
+
+    def get_queryset(self):
+        q = self.request.GET.get('q', '')
+        if not q:
+            return Verse.objects.none()
+        return Verse.objects.filter(
+            Q(verse__icontains=q) | Q(verse_without_tashkeel__icontains=q)
+        )
+
+
+# #3
+class SurahVerses(ListAPIView):
+    """Get all verses of a surah by surah id"""
+    serializer_class = VerseSerializer
+
+    def get_queryset(self):
+        surah_id = self.kwargs['surah_id']
+        surah_prefix = f'S{surah_id:03d}'
+        return Verse.objects.filter(verse_pk__startswith=surah_prefix)
+
+
+# #4
+class VerseDetail(APIView):
+    """Get a specific verse by surah id and verse number"""
+
+    def get(self, request, surah_id, verse_number):
+        verse_pk = f'S{surah_id:03d}V{verse_number:03d}'
+        verse = get_object_or_404(Verse, verse_pk=verse_pk)
+        return Response(VerseSerializer(verse).data)
+
+
+# #5
+class VersesRange(ListAPIView):
+    """Get verses by number_in_quran range"""
+    serializer_class = VerseSerializer
+
+    def get_queryset(self):
+        start = self.kwargs['start']
+        end = self.kwargs['end']
+        if start > end:
+            start, end = end, start
+        return Verse.objects.filter(number_in_quran__range=(start, end))
+
+
+# #6
+class VersesStartsWith(ListAPIView):
+    """Search verses that start with given letters using ?q=letters"""
+    serializer_class = VerseSerializer
+
+    def get_queryset(self):
+        q = self.request.GET.get('q', '')
+        if not q:
+            return Verse.objects.none()
+        return Verse.objects.filter(
+            Q(verse__startswith=q) | Q(verse_without_tashkeel__startswith=q)
+        )
+
+
+# #7
+class VersesEndsWith(ListAPIView):
+    """Search verses that end with given letters using ?q=letters"""
+    serializer_class = VerseSerializer
+
+    def get_queryset(self):
+        q = self.request.GET.get('q', '')
+        if not q:
+            return Verse.objects.none()
+        return Verse.objects.filter(
+            Q(verse__endswith=q) | Q(verse_without_tashkeel__endswith=q)
+        )
+
 
--- verses/urls.py
+++ verses/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
-from .api_views import VersesList
+from .api_views import (
+    VersesList, VersesSearch, SurahVerses, VerseDetail,
+    VersesRange, VersesStartsWith, VersesEndsWith,
+)
 
 app_name = 'verses'
 
 
 urlpatterns = [
 
-
-
     # --> API <--
     path('api/verses_list', VersesList.as_view(), name='verses_list_api'),
+    path('api/search/', VersesSearch.as_view(), name='verses_search_api'),
+    path('api/surah/<int:surah_id>/', SurahVerses.as_view(), name='surah_verses_api'),
+    path('api/verse/<int:surah_id>/<int:verse_number>/', VerseDetail.as_view(), name='verse_detail_api'),
+    path('api/range/<int:start>/<int:end>/', VersesRange.as_view(), name='verses_range_api'),
+    path('api/starts-with/', VersesStartsWith.as_view(), name='verses_starts_with_api'),
+    path('api/ends-with/', VersesEndsWith.as_view(), name='verses_ends_with_api'),
 ]
